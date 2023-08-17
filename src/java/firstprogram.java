7dbeca1f2b8d648365c3ba32315e325ccb255df6
omninawe27 <omninawe27@gmail.com>
2023-08-17 15:49:36 +0530

    java programs

---
+++ src/java/firstprogram.java
@@ -0,0 +1,11 @@
+public class firstprogram {
+    public static void main(String[] args) {
+        System.out.println("welcome to java first program");
+        int a=10;
+        a++;
+        int b=a + 10;
+        System.out.println(b);
+        b = ++a +b +10;
+        System.out.println(b);
+    }
+}
---
+++ src/java/hrstomin.java
@@ -0,0 +1,12 @@
+public class hrstomin {
+    public static void main(String[] args) {
+        int hours =8;
+        int minutes =30;
+        int seconds= minutes * 60;
+        int hourseconds =hours * 60 * 60;
+        System.out.println("minutes to seconds " + seconds);
+        System.out.println("Hour to seconds " + hourseconds);
+        System.out.println(" THANKING YOU" );
+
+    }
+}
